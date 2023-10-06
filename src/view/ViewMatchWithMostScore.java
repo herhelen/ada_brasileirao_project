3f6d511dd880a775fd661567a83a6f4da06c800e
mariannyneris2004 <mariannyneris2004@gmail.com>
2023-10-06 10:36:50 -0300

    Tratamento de retorno null com Optional

--- src/view/ViewMatchWithMostScore.java
+++ src/view/ViewMatchWithMostScore.java
@@ -4,21 +4,28 @@ import src.model.DadosFull;
 import src.service.ServiceFull;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ViewMatchWithMostScore {
     public static void show(ServiceFull serviceFull){
         List<DadosFull> matches = serviceFull.getMatchWithMostScore();
 
-        if (matches.size() > 1){
-            System.out.println("Os placares das partidas com mais gols: ");
+        Optional<List<DadosFull>> optional = Optional.ofNullable(matches);
+
+        if (optional.isPresent()) {
+            if (matches.size() > 1){
+                System.out.println("Os placares das partidas com mais gols: ");
+            } else {
+                System.out.println("O placar da partida com mais gols: ");
+            }
+            // O placar da partida com mais gols.
+            for (DadosFull dadosFull : matches) {
+                System.out.println(
+                        dadosFull.getHost() +  " " + dadosFull.getHostScore() + " X " +
+                                dadosFull.getVisitorScore() + " " + dadosFull.getVisitor());
+            }
         } else {
-            System.out.println("O placar da partida com mais gols: ");
-        }
-        // O placar da partida com mais gols.
-        for (DadosFull dadosFull : matches) {
-            System.out.println(
-                    dadosFull.getHost() +  " " + dadosFull.getHostScore() + " X " +
-                            dadosFull.getVisitorScore() + " " + dadosFull.getVisitor());
+            System.out.println("Não foi possível efetuar a operação.");
         }
     }
 }
