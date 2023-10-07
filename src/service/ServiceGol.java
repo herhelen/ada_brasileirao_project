912b55abaf515be3648a1e804d48adbbbf77151a
Helen Her <her.helen@gmail.com>
2023-10-07 00:04:36 -0300

    apply same refatoring of ServiceCards for ServiceGol

--- src/service/ServiceGol.java
+++ src/service/ServiceGol.java
@@ -5,6 +5,7 @@ import src.repository.Repositorio;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -15,10 +16,10 @@ public class ServiceGol extends Service<DadosGol> {
         super(repositorio);
     }
 
-    public List<Map.Entry<String, Long>> getAthleteWithMostGoals() {
-
+    private List<Map.Entry<String, Long>> getAtheleteWithMostSomething(Predicate<DadosGol> filter) {
         Supplier<Stream<Map.Entry<String, Long>>> baseStream = () -> this.repositorio
                 .getDados().stream()
+                .filter(filter)
                 // agrupa pelo nome do jogador e conta número de gols feitos por ele
                 .collect(Collectors.groupingBy(DadosGol::getAtleta, Collectors.counting()))
                 .entrySet()
@@ -34,58 +35,20 @@ public class ServiceGol extends Service<DadosGol> {
         // Retorna uma lista de jogadores que tenha número de gols igual ao maior número de gols
         return baseStream
                 .get()
-                .filter(entry -> entry.getValue() == numberMostGoals)
+                .filter(entry -> numberMostGoals.equals(entry.getValue()))
                 .collect(Collectors.toList());
+    }
+
+    public List<Map.Entry<String, Long>> getAthleteWithMostGoals() {
+        return getAtheleteWithMostSomething(dadosGol -> true); // não é necessário filtrar
     }
 
     public List<Map.Entry<String, Long>> getAthleteWithMostPenalties() {
-
-        Supplier<Stream<Map.Entry<String, Long>>> baseStream = () -> this.repositorio
-                .getDados().stream()
-                // filtra apenas gols do tipo pênalti
-                .filter(dadosGol -> dadosGol.getTipoDeGol().equalsIgnoreCase("Penalty"))
-                // agrupa pelo nome do jogador e conta número de pênaltis
-                .collect(Collectors.groupingBy(DadosGol::getAtleta, Collectors.counting()))
-                .entrySet()
-                .stream();
-
-        // Encontra o maior número de pênaltis
-        Long numberMostPenalties = baseStream
-                .get()
-                .max(Map.Entry.comparingByValue())
-                .get()
-                .getValue();
-
-        // Retorna uma lista de jogadores que tenha número de pênaltis igual ao maior número de pênaltis
-        return baseStream
-                .get()
-                .filter(entry -> entry.getValue() == numberMostPenalties)
-                .collect(Collectors.toList());
+        return getAtheleteWithMostSomething(dadosGol -> "Penalty".equalsIgnoreCase(dadosGol.getTipoDeGol()));
     }
 
     public List<Map.Entry<String, Long>> getAthleteWithMostOwnGoals() {
-
-        Supplier<Stream<Map.Entry<String, Long>>> baseStream = () -> this.repositorio
-                .getDados().stream()
-                // filtra apenas gols do tipo gol contra
-                .filter(dadosGol -> dadosGol.getTipoDeGol().equalsIgnoreCase("Gol Contra"))
-                // agrupa pelo nome do jogador e conta número de gols contras
-                .collect(Collectors.groupingBy(DadosGol::getAtleta, Collectors.counting()))
-                .entrySet()
-                .stream();
-
-        // Encontra o maior número de gols contras
-        Long numberMostOwnGoals = baseStream
-                .get()
-                .max(Map.Entry.comparingByValue())
-                .get()
-                .getValue();
-
-        // Retorna uma lista de jogadores que tenha número de gols contras igual ao maior número de gols contras
-        return baseStream
-                .get()
-                .filter(entry -> entry.getValue() == numberMostOwnGoals)
-                .collect(Collectors.toList());
+        return getAtheleteWithMostSomething(dadosGol -> "Gol Contra".equalsIgnoreCase(dadosGol.getTipoDeGol()));
     }
 
 }
