2e26486f08b99b591a51d1e1a6ed5fa59e9e925c
Helen Her <her.helen@gmail.com>
2023-10-06 23:57:50 -0300

    refactoring code to use Predicate and extract duplicated code to new method

--- src/service/ServiceCards.java
+++ src/service/ServiceCards.java
@@ -5,6 +5,7 @@ import src.repository.Repositorio;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -15,51 +16,32 @@ public class ServiceCards extends Service<DadosCards> {
         super(repositorio);
     }
 
-    public List<Map.Entry<String, Long>> getAthleteWithMostYellowCards() {
-
+    private List<Map.Entry<String, Long>> getAtheleteWithMostCards(Predicate<DadosCards> filter) {
         Supplier<Stream<Map.Entry<String, Long>>> baseStream = () -> this.repositorio
                 .getDados().stream()
-                .filter(dadosCards -> dadosCards.getCard().equalsIgnoreCase("Amarelo"))
-                // agrupa pelo nome do jogador e conta número de cartões amarelos que ele recebeu
+                .filter(filter)
+                // agrupa pelo nome do jogador e conta número de cartões que ele recebeu
                 .collect(Collectors.groupingBy(DadosCards::getAthleteName, Collectors.counting()))
                 .entrySet()
                 .stream();
 
-        // Encontra o maior número de cartões amarelos
-        Long numberMostYellowCards = baseStream
+        Long maxCards = baseStream
                 .get()
                 .max(Map.Entry.comparingByValue())
                 .get()
                 .getValue();
 
-        // Retorna uma lista de jogadores que tenha número de cartões amarelos igual ao maior número de cartões amarelos
         return baseStream
                 .get()
-                .filter(entry -> entry.getValue() == numberMostYellowCards)
+                .filter(entry -> maxCards.equals(entry.getValue()))
                 .collect(Collectors.toList());
     }
 
+    public List<Map.Entry<String, Long>> getAthleteWithMostYellowCards() {
+        return getAtheleteWithMostCards(dadosCards -> "Amarelo".equalsIgnoreCase(dadosCards.getCard()));
+    }
+
     public List<Map.Entry<String, Long>> getAthleteWithMostRedCards() {
-
-        Supplier<Stream<Map.Entry<String, Long>>> baseStream = () -> this.repositorio
-                .getDados().stream()
-                .filter(dadosCards -> dadosCards.getCard().equalsIgnoreCase("Vermelho"))
-                // agrupa pelo nome do jogador e conta número de cartões vermelhos que ele recebeu
-                .collect(Collectors.groupingBy(DadosCards::getAthleteName, Collectors.counting()))
-                .entrySet()
-                .stream();
-
-        // Encontra o maior número de cartões vermelhos
-        Long numberMostRedCards = baseStream
-                .get()
-                .max(Map.Entry.comparingByValue())
-                .get()
-                .getValue();
-
-        // Retorna uma lista de jogadores que tenha número de cartões vermelhos igual ao maior número de cartões vermelhos
-        return baseStream
-                .get()
-                .filter(entry -> entry.getValue() == numberMostRedCards)
-                .collect(Collectors.toList());
+        return getAtheleteWithMostCards(dadosCards -> "Vermelho".equalsIgnoreCase(dadosCards.getCard()));
     }
 }
